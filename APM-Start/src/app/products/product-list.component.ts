dc75a726257aa4fbe63fa6b32b6aee7a0d754ed0
samruddhipatel <samruddhibpatel@gmail.com>
2020-07-11 21:45:19 +0200

    implementation for async pipe and basic error handling

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,37 +1,34 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription, Observable } from 'rxjs';
+import { Subscription, Observable, EMPTY } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  // products: Product[] = [];
   products$: Observable<Product[]>;
   sub: Subscription;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    // Implementation of async pipe example
-    this.products$ = this.productService.getProducts();
-    // this.sub = this.productService.getProducts()
-    //   .subscribe(
-    //     products => this.products = products,
-    //     error => this.errorMessage = error
-    //   );
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+  // Implementation of async pipe example
+   this.products$ = this.productService.getProducts().pipe(
+     // implementation for error handling with EMPTY
+     catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+     })
+   )
   }
 
   onAdd(): void {
